f269016e03ed566904e87a83d6eb767f44e87005
Fidelis Msacky <fidelismsacky@yahoo.com>
2017-02-26 18:20:56 -0500

    Form functionality added. All edit, add and delete should work.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,18 +6,18 @@ import {Ingredient} from "./shared/ingredient";
 export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe('Parmesan Chicken', 'Fried breaded chicken topped with a white béchamel sauce and cheese', 'http://images.media-allrecipes.com/userphotos/250x250/618489.jpg', [
-      new Ingredient ('Eggs', 2),
-      new Ingredient ('Chicken Breast', 4),
-      new Ingredient ('Grated Parmesan cheese', 1)
+      new Ingredient('Eggs', 2),
+      new Ingredient('Chicken Breast', 4),
+      new Ingredient('Grated Parmesan cheese', 1)
     ]),
     new Recipe('Baby back ribs', 'Grilled pork ribs', 'http://www.amazingtaste.com/wp-content/uploads/2013/10/Margarita-Baby-Back-RIbs-iStock_000018367441Medium.jpg', [
-      new Ingredient ('Full rack of pork ribs', 2),
-      new Ingredient ('Tablespoons kosher salt', 3),
-      new Ingredient ('Tablespoons chili powder', 2)
+      new Ingredient('Full rack of pork ribs', 2),
+      new Ingredient('Tablespoons kosher salt', 3),
+      new Ingredient('Tablespoons chili powder', 2)
     ]),
     new Recipe('Teriyaki Salmon', 'Salmon with teriyaki sauce', 'http://healthyrecipesblogs.com/wp-content/uploads/2014/07/teriyaki-salmon1.jpg', [
-      new Ingredient ('6 ounce salmon fillets', 4),
-      new Ingredient ('1/4 teaspoon garlic powder', 1)
+      new Ingredient('6 ounce salmon fillets', 4),
+      new Ingredient('1/4 teaspoon garlic powder', 1)
     ])
   ];
 
@@ -34,5 +34,13 @@ export class RecipeService {
 
   deleteRecipe(recipe: Recipe) {
     this.recipes.splice(this.recipes.indexOf(recipe), 1)
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+  }
+
+  editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
+    this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
   }
 }
